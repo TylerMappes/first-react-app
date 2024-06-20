6f13186bcb7e752bd7976ef998de998a95ef1d78
TylerMappes <tylermappes@gmail.com>
2024-06-19 22:50:15 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,54 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <nav>
+          <ul>
+            <li>
+              <link to="/">Home</link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  </Router>
+  );
+}
+
+export default App;
